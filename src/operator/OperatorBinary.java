fb247e119df88093584e7e7122bca39643bde5c5
MuhammadArjidan5 <jidan.app1232gmail.com>
2021-04-24 12:37:18 +0700

    Operator Unary

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,21 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Arjidan";
+    int jumlahSks = 24;
+    double indeksPresentasiKomulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
